05ac214f62a5b1047fd205307fc7ce4cb8507cef
Fooxfeir <g223325@dac.unicamp.br>
2024-08-25 16:47:47 -0300

    Changes made on win

--- simple-shell.c
+++ simple-shell.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s PATH\n", argv[0]);
@@ -14,28 +16,103 @@ int main(int argc, char *argv[]) {
     for (char *p = input; *p; p++) {
         if (*p == ':') num_directories++;
     }
-    printf("num_directories: %d\n", num_directories);
 
-    // char **directories = malloc(num_directories * sizeof(char*));
-    // char *start = input;
-    // int dir_index = 0;
-    // for (char *p = input; ; p++) {
-    //     if (*p == ':' || *p == '\0') {
-    //         int length = p - start;
-    //         directories[dir_index] = malloc(length + 1); 
-    //         strncpy(directories[dir_index], start, length);
-    //         directories[dir_index][length] = '\0'; 
-    //         dir_index++;
-    //         start = p + 1;
-    //     }
-    //     if (*p == '\0'){
-    //         break;
-    //     }
-    // }
+    char **directories = malloc(num_directories * sizeof(char*));
+    char *start = input;
+    int dir_index = 0;
+    for (char *p = input; ; p++) {
+        if (*p == ':' || *p == '\0') {
+            int length = p - start;
+            directories[dir_index] = malloc(length + 1); 
+            strncpy(directories[dir_index], start, length);
+            directories[dir_index][length] = '\0'; 
+            dir_index++;
+            start = p + 1;
+        }
+        if (*p == '\0'){
+            break;
+        }
+    }
 
-    // printf("simple-shell$: ");
-    // for (char *directory = directories[0]; directory; directory++) {
-    //     printf("%s\n", directory);
-    // }
+    printf("simple-shell$: ");
+
+    char *line;
+    size_t len = 0;
+    my_getline(&line, &len, stdin);
+    line[strcspn(line, "\n")] = 0;
+
+    char *delimiter = " ";
+    char *token;
+    char **tokens = NULL;
+    int num_tokens = 0;
+
+    token = strtok(line, delimiter);
+    while (token != NULL) {
+        num_tokens++;
+        tokens = realloc(tokens, (num_tokens) * sizeof(char*));
+        tokens[num_tokens - 1] = strdup(token);
+
+        token = strtok(NULL, delimiter);
+    }
+
+    for (int i = 0; i < num_tokens; i++) {
+        printf("Token %d: %s\n", i, tokens[i]);
+    }
+
+    for (int i = 0; i < num_directories; i++) {
+        if (execv(directories[i], tokens) == -1) {
+            continue;
+        }
+    }
+    printf("Command not found\n");
+
+    free(line);
+}
+
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream) {
+    if (lineptr == NULL || n == NULL || stream == NULL) {
+        return -1;
+    }
+
+    char *buf = *lineptr;
+    size_t size = *n;
+
+    if (buf == NULL || size == 0) {
+        size = 128; // Initial buffer size
+        buf = malloc(size);
+        if (buf == NULL) {
+            return -1;
+        }
+    }
+
+    int c;
+    size_t len = 0;
+
+    while ((c = fgetc(stream)) != EOF) {
+        if (len + 1 >= size) {
+            size *= 2;
+            char *new_buf = realloc(buf, size);
+            if (new_buf == NULL) {
+                free(buf);
+                return -1;
+            }
+            buf = new_buf;
+        }
+        buf[len++] = c;
+        if (c == '\n') {
+            break;
+        }
+    }
+
+    if (len == 0) {
+        free(buf);
+        return -1;
+    }
+
+        buf[len] = '\0';
+    *lineptr = buf;
+    *n = size;
+
+    return len;
 }
 
